e3622d8242ed575ac2f39ba08808f6aa656b770f
yamagame <yamagame@mac.com>
2023-07-12 08:29:48 +0900

    クラスに変更

--- src/model/message.ts
+++ src/model/message.ts
@@ -1,4 +1,10 @@
-export type Message = {
+export class Message {
+  id: number
   date: Date
   text: string
+  constructor(props: Message) {
+    this.id = props.id
+    this.date = props.date
+    this.text = props.text
+  }
 }
